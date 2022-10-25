5fa8d72fa5f94e3a0c3ee5fd6e623ec1234ec754
Shakil <shakil.hossain450s@gmail.com>
2022-10-25 10:03:02 +0600

    simple basic structure is done

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+const cors = require("cors");
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Server Side is Running");
+});
+
+app.listen(port, () => {
+  console.log("Server Side is Running on Port : " + port);
+});
